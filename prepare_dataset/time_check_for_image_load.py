769c3696254c0ffb09589fa2149a92f632d2da8b
Safat99 <sarkersafat99@gmail.com>
2021-07-31 07:08:03 +0600

    improved version

--- prepare_dataset/time_check_for_image_load.py
+++ prepare_dataset/time_check_for_image_load.py
@@ -12,6 +12,8 @@ df = df.iloc[:,:1]
 # print(df.head(5))
 # print(df.tail(5))
 
+# class Timecalculate:
+#     img_data, img_data_np, img_data_ls = [],[],[]
 def time_check_with_list():
     img_data = []
     start_time = datetime.now()
@@ -24,28 +26,31 @@ def time_check_with_list():
     total_time = end_time - start_time
     print('time taking for list : {}'.format(total_time))
     print(img_data.shape)
-    
     return total_time
 
 def time_check_with_numpy():
-    img_data_np = np.array([])
+    # img_data_np = np.array([])
+    img_data_np = np.empty([1,224,224,3], dtype='float32')
     start_time = datetime.now()
     for i in df['image']:
         image = load_img(str(i) + '.jpg', target_size=(224,224))
         image = img_to_array(image)
-        img_data_np = np.append(img_data_np, image)
-
-    tmp = img_data_np.shape[0]
-    tmp = tmp / (224 * 224 * 3)
-    img_data_np = np.reshape(img_data_np,(int(tmp),224,224,3))
+        image = np.reshape(image, [1,224,224,3])
+        # image = image[np.newaxis,...]
+        img_data_np = np.append(img_data_np, image,axis=0)
+        # img_data_np = np.append(img_data_np,image)
+    img_data_np = np.delete(img_data_np,0,0) # (arr,index,axis)
+    # tmp = img_data_np.shape[0]
+    # tmp = tmp / (224 * 224 * 3)
+    # img_data_np = np.reshape(img_data_np,(int(tmp),224,224,3))
     end_time = datetime.now()
     total_time = end_time - start_time
     print('time taking for numpy : {}'.format(total_time))
-    print(img_data_np.shape)
-    
+    print(img_data_np.shape)    
     return total_time
 
 def time_check_with_list_comprehension():
+    img_data_ls = []
     start_time = datetime.now()
     def func(i):
         image = load_img(str(i) + '.jpg', target_size=(224,224))
@@ -58,7 +63,6 @@ def time_check_with_list_comprehension():
     print('time taking for list comprehension : {}'.format(total_time))
     print(img_data_ls.shape)
     return total_time
-
 
 
 if __name__ == '__main__':
@@ -84,4 +88,8 @@ time taking for list comprehension : 0:00:00.193769
 0:00:00.193769
 difference is 0:00:00.212061 sec
 differenece of list vs list comprehension is 0:00:00.001978 sec
+'''  
+'''
+In bigger dataset I tried in kaggle notebook where numpy method is significantly slower than
+the other twos! 
 '''
